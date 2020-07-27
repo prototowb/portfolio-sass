2edaec45e70dca1ec5b4bb0cccd75da1f44ebcf0
Tobias Rauer <prototowb@gmail.com>
2020-07-27 19:20:46 +0200

    added lightbox to projects page

--- scripts/main.js
+++ scripts/main.js
@@ -25,3 +25,46 @@ function toggleMenu() {
         showMenu = false;
     }
 }
+
+
+//#################
+//#    Lightbox
+//#################
+
+function openModal() {
+    document.getElementById("myModal").style.display = "block";
+    document.querySelector('nav').style.display = "none";
+}
+
+function closeModal() {
+    document.getElementById("myModal").style.display = "none";
+    document.querySelector('nav').style.display = "block";
+}
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("demo");
+
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
